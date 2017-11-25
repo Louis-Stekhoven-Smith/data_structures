f360edf7e42fbb5a251bcd4c03fe4b4a9fc8384a
Louis <louis.stekhoven@gmail.com>
2017-11-26 00:15:26 +1100

    Single list - implemented add node and remove node

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -7,16 +7,19 @@
 class linkedList(object):
 
 
+    def __init__(self, val):
 
-    def __init__(self):
-        self.head = node()
+        self.head = node(val)
         self.tail = self.head
 
 
+    # Add new node to the end of the list
     def add_node(self, val):
         self.tail.nextNode =  node(val)
         self.tail = self.tail.nextNode
 
+
+    # Removes node with given val
     def remove_node(self, val):
 
         currentNode = self.head
@@ -24,20 +27,76 @@ class linkedList(object):
 
         while(currentNode):
 
+            # Removes node unless
             if(currentNode.val is val):
+
                 currentNode.val = None
-                lastNode.nextNode = currentNode.nextNode
-                break
+
+                # If the node being removed is the head of the list reset head to next node
+                if(currentNode is lastNode):
+                    self.head = currentNode.nextNode
+                    break
+
+                # Remove node
+                else:
+                    lastNode.nextNode = currentNode.nextNode
+                    break
+
 
             lastNode = currentNode
             currentNode = currentNode.nextNode
 
 
-class node():
+    def __str__(self):
+        content = ''
+        currentNode = self.head
 
-    def __init__(self):
+        # How you do a do while loop in python
+        # There is no do while in python
+        while (True):
+            content += (currentNode.val)
+            content += '\n'
+            currentNode = currentNode.nextNode
+
+            if(currentNode is None):
+                break
+
+        return content
+
+
+class node(object):
+
+    def __init__(self,val):
         self.nextNode = None
-        self.val = None
+        self.val = val
+
+
+
+
+list = linkedList('Head')
+
+list.add_node('First')
+list.add_node('Second')
+list.add_node('Third')
+list.add_node('Fourth')
+
+print(list)
+
+list.remove_node('Second')
+
+print(list)
+
+list.remove_node('Head')
+
+print(list)
+
+list.remove_node('Fourth')
+
+print(list)
+
+list.remove_node('doesnt exist')
+
+print(list)
 
 
 
